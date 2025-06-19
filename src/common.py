9811e5c08e46a9123a890737e619723ec51b5733
WSpamer <spamer24310@gmail.com>
2025-06-19 11:00:53 +0200

    feat: add date conversion functions for improved date handling

--- src/common.py
+++ src/common.py
@@ -1,4 +1,14 @@
+import datetime
 import json
+from typing import Optional, Union
+
+import pandas as pd
+from loguru import logger
+
+date_formats = {
+    "short": "%Y-%m-%d",
+    "long": "%Y-%m-%d %H:%M:%S",
+}
 
 
 def read_json(file_path):
@@ -46,6 +56,64 @@ def append_to_json(filename, new_data):
                 file,
                 indent=4,
             )
+
+
+def date_datetime(date: Union[str, pd.Timestamp]) -> Optional[pd.Timestamp]:
+    """
+    Converts a date string to a pandas Timestamp.
+    Args:
+        date (str): The date to convert.
+    Returns:
+        pd.Timestamp: The converted date as a pandas Timestamp, or None if conversion fails.
+    """
+    # .strftime(date_formats["short"])
+    if isinstance(date, str):
+        try:
+            ts = pd.to_datetime(date, errors="coerce")
+            if pd.isna(ts):
+                return None
+            return ts
+        except ValueError:
+            return None  # Return None if conversion fails
+    elif isinstance(date, pd.Timestamp):
+        return date
+
+    else:
+        raise ValueError("Input must be a string or pandas Timestamp.")
+
+
+def date_string(
+    date: Union[str, pd.Timestamp, datetime.date],
+    format: str = "long",
+    date_formats: dict = date_formats,
+) -> str:
+    """
+    Converts a date string to a standardized format.
+    Args:
+        date (str): The date to format.
+    Returns:
+        str: The formatted date string.
+    """
+
+    format = format.lower()
+
+    # Check if format is valid
+    if format not in date_formats and format != "iso":
+        raise ValueError("Invalid format specified. Use 'short', 'long', or 'iso'.")
+    # Convert date to pandas Timestamp
+    if isinstance(date, pd.Timestamp):
+        ts = date
+    if isinstance(date, str):
+        ts = date_datetime(date)
+    elif isinstance(date, datetime.date):
+        ts = pd.Timestamp(date)
+    if ts is not None:
+        return ts.strftime(date_formats[format])
+    else:
+        # If the date is not a valid string or Timestamp, return it as is
+        raise ValueError(
+            f"Invalid date input: {date}. Must be a string or pandas Timestamp."
+        )
 
 
 # Example usage:
