929cd9493ceafb3b33fe8ed48ec7c82822a8da43
WSpamer <spamer24310@gmail.com>
2025-06-26 10:43:21 +0200

    feat: add theme configuration and initialize session state for datetime in index.py

--- streamlit/index.py
+++ streamlit/index.py
@@ -25,11 +25,22 @@ if "import_data" not in st.session_state:
     st.session_state.import_data = pd.DataFrame()
 if "loading_data" not in st.session_state:
     st.session_state.loading_data = False
+
+# Initialize session state for datetime
 if "data_start_date" not in st.session_state:
     st.session_state.data_start_date = None
 if "data_end_date" not in st.session_state:
     st.session_state.data_end_date = None
+
+# Initialize session state for datetime
+if "start_datetime" not in st.session_state:
+    st.session_state.start_datetime = None
+if "end_datetime" not in st.session_state:
+    st.session_state.end_datetime = None
+
 pg = st.navigation([import_page, live_page, historic_page, chill_page])
-st.set_page_config(page_title="Data manager", page_icon=":material/edit:")
+st.set_page_config(
+    page_title="Data manager", page_icon=":material/edit:", layout="wide"
+)
 # st.sidebar.header("Navigation")
 pg.run()
