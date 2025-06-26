d91a6b15b2906b2d4643ec7841c2d4bc5cb1cf5e
Wian Spamer <61464034+WSpamer@users.noreply.github.com>
2025-06-26 10:45:37 +0200

    refactor: replace hardcoded project path with dynamic retrieval based on OS

--- src/env.py
+++ src/env.py
@@ -4,7 +4,23 @@ from loguru import logger
 
 from common import read_json
 
-project_path = "/Users/wspamer/Dev/python/projects/ccr-system/nvirosense"
+# get_project_path() = "/Users/wspamer/Dev/python/projects/ccr-system/nvirosense"
+
+
+def get_project_path(OS="WSL") -> str:
+    """
+    Returns the project path.
+    """
+    if OS not in ["WSL", "Mac"]:
+        logger.error(f"Unsupported OS: {OS}. Supported OS are 'WSL' and 'Mac'.")
+        raise ValueError(f"Unsupported OS: {OS}. Supported OS are 'WSL' and 'Mac'.")
+    if OS == "WSL":
+        return "/home/wspamer/dev/python/nvirosense"
+    elif OS == "Mac":
+        return "/Users/wspamer/Dev/python/projects/ccr-system/nvirosense"
+    else:
+        logger.error(f"Unsupported OS: {OS}. Supported OS are 'WSL' and 'Mac'.")
+        raise ValueError(f"Unsupported OS: {OS}. Supported OS are 'WSL' and 'Mac'.")
 
 
 def read_global(file_path: str = "global.json") -> dict:
@@ -13,7 +29,7 @@ def read_global(file_path: str = "global.json") -> dict:
     Returns:
         dict: Global settings loaded from the JSON file.
     """
-    file_path = os.path.join(project_path, file_path)
+    file_path = os.path.join(get_project_path(), file_path)
     if not os.path.exists(file_path):
         raise FileNotFoundError(f"Global settings file '{file_path}' not found.")
     return read_json(file_path)
@@ -29,12 +45,12 @@ def env_global(name: str = "start_date", file_path: str = "global.json") -> str:
         logger.error("Global settings are empty or not properly loaded.")
         raise ValueError("Global settings are empty or not properly loaded.")
     if name == "project_path":
-        return project_path
+        return get_project_path()
     if name not in global_settings:
         logger.error(f"Global settings must contain '{name}' key.")
         raise KeyError(f"Global settings must contain '{name}' key.")
     if name == "path_data":
-        global_settings[name] = os.path.join(project_path, global_settings[name])
+        global_settings[name] = os.path.join(get_project_path(), global_settings[name])
         logger.info(f"Updated 'path_data' to: {global_settings[name]}")
 
     return global_settings[name]
